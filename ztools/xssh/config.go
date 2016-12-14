0a890399a9871f953ced2764076d08a5ca125443
Drew Springall <aaspring@umich.edu>
2016-12-14 14:40:26 -0500

    Add --xssh-host-key-algs flag.

--- ztools/xssh/config.go
+++ ztools/xssh/config.go
@@ -2,8 +2,9 @@ package xssh
 
 func MakeXSSHConfig() *ClientConfig {
 	ret := &ClientConfig{
-		DontAuthenticate: true, // IOT scan ethically, never attempt to authenticate
-		ClientVersion:    pkgConfig.ClientID,
+		DontAuthenticate:  true, // IOT scan ethically, never attempt to authenticate
+		ClientVersion:     pkgConfig.ClientID,
+		HostKeyAlgorithms: pkgConfig.HostKeyAlgorithms.GetStringSlice(),
 	}
 
 	return ret
--- ztools/xssh/flag.go
+++ ztools/xssh/flag.go
@@ -1,15 +1,59 @@
 package xssh
 
 import (
+	"errors"
 	"flag"
+	"fmt"
+	"strings"
 )
-
-type XSSHConfig struct {
-	ClientID string
-}
 
 var pkgConfig XSSHConfig
 
+type XSSHConfig struct {
+	ClientID          string
+	HostKeyAlgorithms HostKeyAlgorithmsList
+}
+
+type HostKeyAlgorithmsList struct {
+	IsSet      bool
+	Algorithms []string
+}
+
+func (hkaList *HostKeyAlgorithmsList) String() string {
+	return "BROKEN HostKeyAlgorithmsList.String()"
+}
+
+func (hkaList *HostKeyAlgorithmsList) Set(value string) error {
+	hkaList.IsSet = true
+	for _, alg := range strings.Split(value, ",") {
+		isValid := false
+		for _, val := range supportedHostKeyAlgos {
+			if val == alg {
+				isValid = true
+				break
+			}
+		}
+
+		if !isValid {
+			return errors.New(fmt.Sprintf(`Can not support host key algorithm : "%s"`, alg))
+		}
+
+		hkaList.Algorithms = append(hkaList.Algorithms, alg)
+	}
+	return nil
+}
+
+func (hkaList *HostKeyAlgorithmsList) GetStringSlice() []string {
+	if !hkaList.IsSet {
+		return supportedHostKeyAlgos
+	} else {
+		return hkaList.Algorithms
+	}
+}
+
 func init() {
 	flag.StringVar(&pkgConfig.ClientID, "xssh-client-id", packageVersion, "Specify the client ID string to use")
+
+	hostKeyAlgUsage := fmt.Sprintf("Which host key algorithms to support (default \"%s\")", strings.Join(supportedHostKeyAlgos, ","))
+	flag.Var(&pkgConfig.HostKeyAlgorithms, "xssh-host-key-algs", hostKeyAlgUsage)
 }
